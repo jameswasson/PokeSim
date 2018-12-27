432baa0921583e80e0907ec73ae102a7744ff669
James Wasson <jameswasson@users.noreply.github.com>
2018-12-26 18:38:00 -0700

    Whirlwind
    Whirlwind implemented early. I learned what it does, which is suprisingly nothing.

--- Game/src/AttackStates/Moves/Whirlwind.java
+++ Game/src/AttackStates/Moves/Whirlwind.java
@@ -1,7 +1,12 @@
 package AttackStates.Moves;
 
 import AttackStates.Move;
+import Pokemons.Pokemon;
 
 public class Whirlwind extends Move {
-    //todo
+    @Override
+    public void attack(Pokemon ourselves, Pokemon opponent, int damage) {
+        logger.println("But it failed!");
+        //This move always fails, ha
+    }
 }
