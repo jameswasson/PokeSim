97bb9d2520773b1cc8a336c026ed3918f55f4025
James Wasson <jameswasson@users.noreply.github.com>
2019-06-15 23:51:28 -0700

    HyperBeam

--- Game/src/attack_states/moves/HyperBeam.java
+++ Game/src/attack_states/moves/HyperBeam.java
@@ -1,7 +1,26 @@
 package attack_states.moves;
 
 import attack_states.Move;
+import pokemons.Pokedex;
+import pokemons.Pokemon;
+import pokemons.WrapperPokemon;
 
 public class HyperBeam extends Move {
-    //todo
+    //todo let HyperBeam penetrate Substitutes.
+    @Override
+    public void attack(Pokemon ourselves, Pokemon opponent, int damage) {
+        super.attack(ourselves, opponent, damage);
+        WrapperPokemon.wrap(ourselves, new BeamChargingPokemon());
+        logger.println(ourselves.getName() + " must recharge!");
+        ourselves.setShouldSelectMove(false);
+    }
+
+    public class BeamChargingPokemon extends WrapperPokemon {
+        @Override
+        public void attack(Pokemon toAttack) {
+            logger.println(getName() + " is recharging!");
+            setShouldSelectMove(true);
+            removeSelf();
+        }
+    }
 }
